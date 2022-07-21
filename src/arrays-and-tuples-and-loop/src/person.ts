9ec9aca14000118738f1601b36253bdfc29eb8b0
leedongyeop <ldy_1204@naver.com>
2022-07-22 00:30:49 +0900

    반복문 / for ... in 문

---
+++ src/arrays-and-tuples-and-loop/src/person.ts
@@ -0,0 +1,27 @@
+export default class Person {
+    static numberOfPerson: number = 0;
+
+    constructor(
+        private readonly name: string, //생성자에서는 접근제한자를 무조건 써주기
+        private readonly age: number,  //readonly 키워드도 따로 정리하기
+        private readonly etc: boolean,
+    ) {
+        Person.numberOfPerson++;
+    }
+
+    printInfo(): void {
+        console.log(`Person name: ${this.name}`);
+        console.log(`Person age: ${this.age}`);
+        console.log(`Person etc: ${this.etc}`);
+    }
+
+    static printNumberOfPerson(): void {
+        console.log(`The Number Of Person Object: ${this.numberOfPerson}`);
+    }
+}
+
+type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
+
+export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
+    return new Person(name, age, etc);
+};
