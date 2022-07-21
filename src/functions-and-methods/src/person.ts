17b6e30140ce116e8cc0c874b88fa86a4ac7ca69
leedongyeop <ldy_1204@naver.com>
2022-07-21 23:31:52 +0900

    함수 구현 기법/객체 생성 시 값 부분 생략

--- src/functions-and-methods/src/person.ts
+++ src/functions-and-methods/src/person.ts
@@ -4,12 +4,10 @@ type Person = {
     etc?: boolean
 };
 
-export function makePerson(name: string, age: number, etc: boolean = false): Person {
-    const person: Person = {
-        name: name,
-        age: age,
-        etc: etc
-    }
+type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
+
+export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
+    const person: Person = { name, age, etc }; //속성 이름과 매개 변수 이름이 같을 때만 적용 가능
 
     return person;
 }
