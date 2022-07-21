8bc14666dd45c8535eeb9f10878ef0896b3d6496
leedongyeop <ldy_1204@naver.com>
2022-07-21 23:51:39 +0900

    클래스 메서드

--- src/functions-and-methods/src/index.ts
+++ src/functions-and-methods/src/index.ts
@@ -1,12 +1,12 @@
 import {printHello} from "./print.hello";
 import {printUser} from "./print.user";
-import {makePerson, printPerson} from "./person";
+import {makePerson} from "./person";
 import {init} from "./init";
 
 printHello();
 printUser('Dongvelop', 24);
 
 const person = makePerson('Dongvelop', 24);
-printPerson(person);
+person.printinfo();
 
 init(() => console.log('custom initialization finished'));
