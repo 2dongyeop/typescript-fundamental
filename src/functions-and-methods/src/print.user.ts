f15ec2589daa63dbe453a91777535b246c5473c6
leedongyeop <ldy_1204@naver.com>
2022-07-21 23:09:14 +0900

    함수 시그니처를 이해하기 위한 실습 코드

---
+++ src/functions-and-methods/src/print.user.ts
@@ -0,0 +1,4 @@
+export const printUser: (arg0: string, arg1: number) => void = function (name: string, age: number): void {
+    console.log(`User's name: ${name}`);
+    console.log(`User's age: ${age}`);
+}
