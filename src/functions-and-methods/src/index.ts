f2c5a4fa4fb4877f92628f846a7f171d50ce6ba8
leedongyeop <ldy_1204@naver.com>
2022-07-21 23:27:16 +0900

    선택적 매개변수와 매개변수의 기본값을 제공해주는 기능

--- src/functions-and-methods/src/index.ts
+++ src/functions-and-methods/src/index.ts
@@ -1,5 +1,9 @@
 import {printHello} from "./print.hello";
 import {printUser} from "./print.user";
+import {makePerson} from "./person";
 
 printHello();
 printUser('Dongvelop', 24);
+
+const person = makePerson('Dongvelop', 24);
+console.log(person);
