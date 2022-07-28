ca0a5b31d93272b695e5400159e34a5dba0ad20f
leedongyeop <ldy_1204@naver.com>
2022-07-28 14:32:06 +0900

    매개변수에 readonly를 붙이려면 매개변수용 클래스를 하나 만들어 주어야 함

--- src/arrays-and-tuples-and-loop/src/person.ts
+++ src/arrays-and-tuples-and-loop/src/person.ts
@@ -22,6 +22,22 @@ export default class Person {
 
 type MakePerson = (arg0: string, arg1: number, arg2?: boolean) => Person;
 
-export const makePerson: MakePerson = (name: string, age: number, etc: boolean = false): Person => {
-    return new Person(name, age, etc);
-};
+class MakePersonData {
+    private _name: string;
+    age: number;
+    etc: boolean;
+
+    public get name(): string {
+        return this._name;
+    }
+    // 아래와 같이 setter는 당연히 readonly가 없어야 함
+    // public set name(name: string): void {
+    //     this._name = _ame;
+    // }
+}
+
+export const makePerson: (personData: Readonly<MakePersonData>) => Person = (personData: Readonly<MakePersonData>): Person => {
+    console.log(personData.name); //_name으로 선언을 해놓았지만 getter 메소드를 통해 호출 가능
+
+    return new Person(personData.name, personData.age, personData.etc);
+}; //매개변수에 readonly를 붙이려면 매개변수용 클래스를 하나 만들어준다? - 제네릭을 이용!!
